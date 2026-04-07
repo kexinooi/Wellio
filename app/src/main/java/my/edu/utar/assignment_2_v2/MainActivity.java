5ac0d26f0a340efcd5640fac2132202f104e4e0b
jyfan <jyfan03@1utar.my>
2026-04-07 17:41:04 +0800

    test

--- app/src/main/java/my/edu/utar/assignment_2_v2/MainActivity.java
+++ app/src/main/java/my/edu/utar/assignment_2_v2/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-bihviyggikk
+
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
 
         bottomNav.setOnItemSelectedListener(item -> {
